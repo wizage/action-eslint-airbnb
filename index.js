09e6914d0e8c977f4b4409fe05b4b8a1f58c071e
wizage <wizagesmax@gmail.com>
2019-11-26 21:33:43 -0800

    Eslint done

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const request = require('./request')
+/* eslint-disable no-restricted-syntax */
 const core = require('@actions/core');
-const { GITHUB_SHA, GITHUB_EVENT_PATH, GITHUB_WORKSPACE } = process.env
-const event = require(GITHUB_EVENT_PATH)
-const { repository } = event
-const {
-  owner: { login: owner }
-} = repository
-const { name: repo } = repository
+const request = require('./request');
 
-const checkName = 'ESLint check'
+const { GITHUB_SHA, GITHUB_EVENT_PATH, GITHUB_WORKSPACE } = process.env;
+const event = require(GITHUB_EVENT_PATH);
+const { repository } = event;
+const {
+  owner: { login: owner },
+} = repository;
+const { name: repo } = repository;
+
+const checkName = 'ESLint check';
 
 const GITHUB_TOKEN = core.getInput('repo-token');
 
@@ -16,50 +18,50 @@ const headers = {
   'Content-Type': 'application/json',
   Accept: 'application/vnd.github.antiope-preview+json',
   Authorization: `Bearer ${GITHUB_TOKEN}`,
-  'User-Agent': 'eslint-action'
-}
+  'User-Agent': 'eslint-action',
+};
 
 async function createCheck() {
   const body = {
     name: checkName,
     head_sha: GITHUB_SHA,
     status: 'in_progress',
-    started_at: new Date()
-  }
+    started_at: new Date(),
+  };
 
   const { data } = await request(`https://api.github.com/repos/${owner}/${repo}/check-runs`, {
     method: 'POST',
     headers,
-    body
-  })
-  const { id } = data
-  return id
+    body,
+  });
+  const { id } = data;
+  return id;
 }
 
 function eslint() {
-  const eslint = require('eslint')
-
-  const cli = new eslint.CLIEngine()
-  const report = cli.executeOnFiles(['.'])
+  const cli = new eslint.CLIEngine();
+  const report = cli.executeOnFiles(['.']);
   // fixableErrorCount, fixableWarningCount are available too
-  const { results, errorCount, warningCount } = report
+  const { results, errorCount, warningCount } = report;
 
-  const levels = ['', 'warning', 'failure']
+  const levels = ['', 'warning', 'failure'];
 
-  const annotations = []
+  const annotations = [];
   for (const result of results) {
-    const { filePath, messages } = result
-    const path = filePath.substring(GITHUB_WORKSPACE.length + 1)
+    const { filePath, messages } = result;
+    const path = filePath.substring(GITHUB_WORKSPACE.length + 1);
     for (const msg of messages) {
-      const { line, severity, ruleId, message } = msg
-      const annotationLevel = levels[severity]
+      const {
+        line, severity, ruleId, message,
+      } = msg;
+      const annotationLevel = levels[severity];
       annotations.push({
         path,
         start_line: line,
         end_line: line,
         annotation_level: annotationLevel,
-        message: `[${ruleId}] ${message}`
-      })
+        message: `[${ruleId}] ${message}`,
+      });
     }
   }
 
@@ -68,16 +70,16 @@ function eslint() {
     output: {
       title: checkName,
       summary: `${errorCount} error(s), ${warningCount} warning(s) found`,
-      annotations
-    }
-  }
+      annotations,
+    },
+  };
 }
 
 async function updateCheck(id, conclusion, output) {
-  let { annotations } = output;
+  const { annotations } = output;
   while (annotations.length >= 50) {
-    let newAnnotations = annotations.splice(0, 50);
-    let newOutput = output;
+    const newAnnotations = annotations.splice(0, 50);
+    const newOutput = output;
     newOutput.annotations = newAnnotations;
     const body = {
       name: checkName,
@@ -88,7 +90,7 @@ async function updateCheck(id, conclusion, output) {
     await request(`https://api.github.com/repos/${owner}/${repo}/check-runs/${id}`, {
       method: 'PATCH',
       headers,
-      body
+      body,
     });
   }
 
@@ -107,30 +109,30 @@ async function updateCheck(id, conclusion, output) {
     method: 'PATCH',
     headers,
     body,
-  })
+  });
 }
 
 function exitWithError(err) {
-  console.error('Error', err.stack)
+  console.error('Error', err.stack);
   if (err.data) {
-    console.error(err.data)
+    console.error(err.data);
   }
-  process.exit(1)
+  process.exit(1);
 }
 
 async function run() {
-  const id = await createCheck()
+  const id = await createCheck();
   try {
-    const { conclusion, output } = eslint()
-    console.log(output.summary)
-    await updateCheck(id, conclusion, output)
+    const { conclusion, output } = eslint();
+    console.log(output.summary);
+    await updateCheck(id, conclusion, output);
     if (conclusion === 'failure') {
-      process.exit(78)
+      process.exit(78);
     }
   } catch (err) {
-    await updateCheck(id, 'failure')
-    exitWithError(err)
+    await updateCheck(id, 'failure');
+    exitWithError(err);
   }
 }
 
-run().catch(exitWithError)
+run().catch(exitWithError);
