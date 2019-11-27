0d057296e43a0c506a3a122cbe7b545cd9bb2434
wizage <wizagesmax@gmail.com>
2019-11-26 19:48:17 -0800

    added new stuff

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require('fs');
+//const fs = require('fs');
 const { GITHUB_WORKSPACE } = process.env
+const eslint = require('eslint');
 
 try {
-  var files = fs.readdirSync(GITHUB_WORKSPACE);
-  console.log(files, GITHUB_WORKSPACE);
+  // var files = fs.readdirSync(GITHUB_WORKSPACE);
+
+  const cli = new eslint.CLIEngine()
+
+  const report = cli.executeOnFiles([`${GITHUB_WORKSPACE}/.`]);
+
+  console.log(report);
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
