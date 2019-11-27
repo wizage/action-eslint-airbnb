49faf2bcd370c6dcf6a49a34e572e15092fd629a
wizage <wizagesmax@gmail.com>
2019-11-26 19:51:14 -0800

    running eslint

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
   const cli = new eslint.CLIEngine();
   console.log("here");
 
-  const report = cli.executeOnFiles([GITHUB_WORKSPACE]);
+  const report = cli.executeOnFiles(['.']);
 
   console.log(report);
   // `who-to-greet` input defined in action metadata file
